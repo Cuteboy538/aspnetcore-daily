66f7ce72563f29513c7ac8d22792c1d6bef3cd61
Sajjad Ahmad <34192966+Cuteboy538@users.noreply.github.com>
2025-07-30 14:40:33 +0500

    Update DecimalExtensions.cs

--- decimal-rounding/DecimalExtensions.cs
+++ decimal-rounding/DecimalExtensions.cs
@@ -1,1 +1,22 @@
+[HtmlTargetElement("input", Attributes = "asp-for")]
+public class DecimalFormatTagHelper : TagHelper
+{
+    [HtmlAttributeName("asp-for")]
+    public ModelExpression For { get; set; }
+    public string TotalDecimals = "0.########";
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        var model = For?.Model;
+
+        if (model is decimal decimalValue)
+        {
+            output.Attributes.SetAttribute("value", decimalValue.ToString(TotalDecimals, CultureInfo.InvariantCulture));
+        }
+        else if (model is decimal? nullableDecimalValue && nullableDecimalValue.HasValue)
+        {
+            output.Attributes.SetAttribute("value", nullableDecimalValue.Value.ToString(TotalDecimals, CultureInfo.InvariantCulture));
+        }
+    }
+}
 
